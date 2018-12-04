d89a38f05b7b9455b63d020ee1534e882ecc5397
jtsimoes <jtsimoes10@outlook.com>
2018-12-04 20:36:50 +0000

    Added level 2 Moved JavaScript to an external file 'code.js'

---
+++ js/code.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#404").click(function(){
+        alert("Don't be too happy, this one was very easy actually. You suck at this if you take more that 1 minute to get here.\nGood luck for the next one...");
+        location.href="quiet";
+    });
+});
+
+$(document).ready(function(){
+    $("#glitch").click(function(){
+        alert("I only have this until now, but I think you are getting the point. Be patient and thanks for playing.");
+    });
+});
+
